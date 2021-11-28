838ab06ff8956b376b23910abfe699830914714d
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-11-28 11:18:21 -0300

    Concluir a tarefa SequenciaLimites

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SequenciaLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o numero inicial da sequência..:");
+            int primeiro = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o numero final da sequência....:");
+            int segundo = Convert.ToInt32(Console.ReadLine());
+
+            if (segundo >= primeiro)
+            {
+                int n = primeiro;
+                while (n <= segundo)
+                {
+                    Console.Write($"{n} ");
+                    n += 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é possível realizar a sequência");
+            }
+
+        }
+    }
+}
